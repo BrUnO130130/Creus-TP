35055619eb2d6f519f8447ed1b2a69dbede64edc
Bruno <xxxnikgamexxx@gmail.com>
2026-04-16 19:47:54 -0300

    creacion de clase Cuenta y interfaz ITransferible, con algunas propiedades

--- obj/Debug/net10.0/HolaMundo.AssemblyInfo.cs
+++ obj/Debug/net10.0/HolaMundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce915568a30db5eaa4bcc7d000a5473ab77f8155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b25a6a4a6abaa9605e1e2532d05ac349616af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
